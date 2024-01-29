dad9c3c709343574b2921e19198d4114c44787c3
jp-rad <jp-rad@example.com>
2024-01-29 04:54:08 +0000

    rename files

--- enums.d.ts
+++ enums.d.ts
@@ -1,4 +1,39 @@
 // Auto-generated. Do not edit.
 
 
+    /**
+     * micro:bit Message Bus ID of listener : 1-65535
+     * Custom Message Bus ID : 32768-65535
+     */
+
+    declare const enum CUSTOM_BUS_ID
+    {
+    /**
+     * S3Link UDK Event Bus ID.
+     * (32768 + 1024 + 1 = 33793)
+     */
+    CUSTOM_EVENT_ID_S3LINK_UDK = 33793,
+    // https://github.com/jp-rad/pxt-s3link-udk/
+
+    /**
+     * MState Update Event Bus ID.
+     * (32768 + 1024 + 5 = 33797)
+     */
+    CUSTOM_EVENT_ID_MSTATE_UPDATE = 33797,
+    // https://github.com/jp-rad/pxt-mstate/
+
+    /**
+     * Idle-Timer Interval Event Bus ID.
+     * (32768 + 1024 + 9 = 33801)
+     */
+    CUSTOM_EVENT_ID_IDLETIMER_INTERVAL = 33801,
+    /**
+     * Idle-Timer Timeout Event Bus ID.
+     * (32768 + 1024 + 10 = 33802)
+     */
+    CUSTOM_EVENT_ID_IDLETIMER_TIMEOUT = 33802,
+    // https://github.com/jp-rad/pxt-idle-timer/
+
+    }
+
 // Auto-generated. Do not edit. Really.
